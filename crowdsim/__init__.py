1122a5333f8600055e15bab195b5bc35306cc09b
Xiaojie Lin <lxj2048@gmail.com>
2014-06-05 15:52:33 +1000

    add evaluate() and linkAll()

--- crowdsim/__init__.py
+++ crowdsim/__init__.py
@@ -1,3 +1,37 @@
 #!/usr/bin/env python3
 
 from . import common, generator, solver, deducer
+
+def evaluate(tasks, answers, strict = False):
+    """evaluate the answers
+
+    if strict == True, will raise ValueError if there are duplicated tasks or
+    each task does not have exactly one answer
+    """
+
+    if strict:
+        taskSet = set()
+        for t in tasks:
+            if t.ID in taskSet:
+                raise ValueError('Duplicated tasks')
+            taskSet.add(t.ID)
+        errString = 'tasks and answers do not match'
+        for a in answers:
+            if a.taskID not in taskSet:
+                raise ValueError(errString)
+            taskSet.remove(a.taskID)
+        if len(taskSet) != 0:
+            raise ValueError(errString)
+
+    taskDict = {t.ID : t.trueOption for t in tasks}
+    count = 0
+    for a in answers:
+        if a.choice == taskDict[a.taskID]:
+            count += 1
+    return count / len(answers)
+
+def linkAll(generator = None, solver = None, deducer = None):
+    if solver:
+        solver.link(generator)
+    if deducer:
+        deducer.link(generator, solver)
