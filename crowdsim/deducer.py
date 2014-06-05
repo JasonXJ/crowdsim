5e6e02e778031b9a1f72038841fa2320371eabca
Xiaojie Lin <lxj2048@gmail.com>
2014-06-05 14:16:23 +1000

    add cache parameter

--- crowdsim/deducer.py
+++ crowdsim/deducer.py
@@ -2,19 +2,22 @@
 from .common import *
 
 class MajorityVote:
+    def __init__(cache = True):
+        # actually, MajorityVote always caches in spite of the parameter
+        pass
     def link(self, generator, solver):
         self.taskDict = {}
         for a in solver:
             if a.taskID not in self.taskDict:
                 self.taskDict[a.taskID] = [0 for x in range(generator.getOptionCount(a.taskID))]
             self.taskDict[a.taskID][a.choice] += 1
-        self.deducedAnswers = []
+        self.cache = []
         for ID in sorted(self.taskDict.keys()):
             mValue = -1
             for c, v in enumerate(self.taskDict[ID]):
                 if v > mValue:
                     mValue = v
                     mChoice = c
-            self.deducedAnswers.append(SimpleAnswer(ID, mChoice))
+            self.cache.append(SimpleAnswer(ID, mChoice))
     def __iter__(self):
-        return iter(self.deducedAnswers)
+        return iter(self.cache)
